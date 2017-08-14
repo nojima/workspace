3d551ebc9c6aadc9e4915e357de351f0874f3a81
Yusuke Nojima <nojima@ynojima.com>
2017-08-15 02:05:59 +0900

    Import word2vec.evaluate in __init__.py

--- learning-chainer/word2vec/__init__.py
+++ learning-chainer/word2vec/__init__.py
@@ -1,5 +1,6 @@
 from word2vec.adhoc import *
 from word2vec.dataset import *
+from word2vec.evaluate import *
 from word2vec.models import *
 from word2vec.train import *
 from word2vec.visualize import *
