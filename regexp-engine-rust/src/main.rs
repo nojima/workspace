187142ab72112a05228ddbabe3b61448b5a1f6c0
Yusuke Nojima <nojima@ynojima.com>
2018-05-07 04:06:54 +0900

    feat(regexp-engine-rust): start implmenting

---
+++ regexp-engine-rust/src/main.rs
@@ -0,0 +1,96 @@
+#![feature(box_syntax)]
+
+extern crate either;
+
+use std::iter;
+use either::{Left, Right};
+
+type Positions<'a> = Box<Iterator<Item=usize> + 'a>;
+
+trait Matcher {
+    fn match_<'a>(&'a self, s: &'a str) -> Positions<'a>;
+}
+
+//-----------------------------------------------------------------------------
+// ZeroMatcher (matches to empty string)
+
+struct ZeroMatcher;
+
+impl ZeroMatcher {
+    fn new() -> ZeroMatcher {
+        ZeroMatcher{}
+    }
+}
+
+impl Matcher for ZeroMatcher {
+    fn match_<'a>(&'a self, s: &'a str) -> Positions<'a> {
+        box iter::once(0)
+    }
+}
+
+//-----------------------------------------------------------------------------
+// CharacterMatcher (matches to specified single character)
+
+struct CharacterMatcher {
+    ch: char,
+}
+
+impl CharacterMatcher {
+    fn new(ch: char) -> CharacterMatcher {
+        CharacterMatcher{
+            ch: ch,
+        }
+    }
+}
+
+impl Matcher for CharacterMatcher {
+    fn match_<'a>(&'a self, s: &'a str) -> Positions<'a> {
+        let expected = self.ch;
+        box s.chars().take(1).filter_map(move |c|
+            if c == expected { Some(1) } else { None }
+        )
+    }
+}
+
+//-----------------------------------------------------------------------------
+// AnyCharacterMatcher (macthes to any single character)
+
+struct AnyCharacterMatcher;
+
+impl AnyCharacterMatcher {
+    fn new() -> AnyCharacterMatcher {
+        AnyCharacterMatcher{}
+    }
+}
+
+impl Matcher for AnyCharacterMatcher {
+    fn match_<'a>(&'a self, s: &'a str) -> Positions<'a> {
+        box s.chars().take(1).map(|_| 1)
+    }
+}
+
+//-----------------------------------------------------------------------------
+
+struct RepeatMatcher {
+    inner: Box<Matcher>,
+}
+
+impl RepeatMatcher {
+    fn new(inner: Box<Matcher>) -> RepeatMatcher {
+        RepeatMatcher{
+            inner: inner,
+        }
+    }
+}
+
+impl Matcher for RepeatMatcher {
+    fn match_<'a>(&'a self, s: &'a str) -> Positions<'a> {
+        box self.inner.match_(s).flat_map(move |n1|
+            if n1 == 0 {
+                Left(iter::once(0 as usize))
+            } else {
+                Right(self.match_(&s[n1..]).map(|n2| n1 + n2))
+            }
+        )
+    }
+}
