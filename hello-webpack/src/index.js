8c924ec92c6e22059229e5dedc2cacf2ae35c7d0
Yusuke Nojima <nojima@ynojima.com>
2020-02-21 23:04:34 +0900

    [hello-webpack] assets

--- hello-webpack/src/index.js
+++ hello-webpack/src/index.js
@@ -1,9 +1,18 @@
 import _ from "lodash";
+import "./style.css";
+//import Zelda from "./zelda.jpg";
 
 function component() {
     const element = document.createElement("div");
 
     element.innerHTML = _.join(["Hello", "webpack"], " ");
+    element.classList.add("hello");
+
+    // Add the image to our existing div.
+    //const myImage = new Image();
+    //myImage.src = Zelda;
+
+    //element.appendChild(myImage);
 
     return element;
 }
--- hello-webpack/webpack.config.js
+++ hello-webpack/webpack.config.js
@@ -3,7 +3,24 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use: [
+                    "file-loader",
+                ],
+            },
+        ],
     },
 };
