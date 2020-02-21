976e18c98e1b09a2cef7fbb140fd6be5d9b80e48
Yusuke Nojima <nojima@ynojima.com>
2020-02-21 23:14:54 +0900

    [hello-webpack] output management

--- hello-webpack/src/index.js
+++ hello-webpack/src/index.js
@@ -1,18 +1,17 @@
 import _ from "lodash";
-import "./style.css";
-//import Zelda from "./zelda.jpg";
+import printMe from "./print.js";
 
 function component() {
     const element = document.createElement("div");
+    const btn = document.createElement("button");
 
     element.innerHTML = _.join(["Hello", "webpack"], " ");
     element.classList.add("hello");
 
-    // Add the image to our existing div.
-    //const myImage = new Image();
-    //myImage.src = Zelda;
+    btn.innerHTML = "Click me and check the console!";
+    btn.onclick = printMe;
 
-    //element.appendChild(myImage);
+    element.appendChild(btn);
 
     return element;
 }
