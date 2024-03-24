b1bab76d28530e928f0bd280b8bdfd1bdd6fc716
Yusuke Nojima <nojima@ynojima.com>
2024-03-24 17:51:12 +0900

    io-monad-in-rust: make Cached safe

--- io-monad-in-rust/src/cached.rs
+++ io-monad-in-rust/src/cached.rs
@@ -1,38 +1,25 @@
-use std::{cell::OnceCell, mem::ManuallyDrop};
+use std::cell::{Cell, OnceCell};
 
 pub struct Cached<T> {
-    init: Box<ManuallyDrop<dyn UnsafeFnOnce<T>>>,
+    init: Cell<Option<Box<dyn FnOnce() -> T>>>,
     value: OnceCell<T>,
 }
 
 impl<T> Cached<T> {
-    pub fn new(action: impl FnOnce() -> T + 'static) -> Self {
+    pub fn new(init: impl FnOnce() -> T + 'static) -> Self {
         Self {
-            init: Box::new(ManuallyDrop::new(action)),
+            init: Cell::new(Some(Box::new(init))),
             value: OnceCell::new(),
         }
     }
 
     pub fn get(&self) -> &T {
-        self.value.get_or_init(|| unsafe { (*self.init).call() })
-    }
-}
-
-impl<T> Drop for Cached<T> {
-    fn drop(&mut self) {
-        if self.value.get().is_none() {
-            unsafe { ManuallyDrop::drop(&mut self.init) }
-        }
-    }
-}
-
-trait UnsafeFnOnce<T>: FnOnce() -> T {
-    unsafe fn call(&self) -> T;
-}
-
-impl<T, F: FnOnce() -> T> UnsafeFnOnce<T> for F {
-    unsafe fn call(&self) -> T {
-        std::ptr::read(self)()
+        self.value.get_or_init(|| {
+            let Some(init) = self.init.take() else {
+                panic!("[bug] init function called twice!")
+            };
+            init()
+        })
     }
 }
 
