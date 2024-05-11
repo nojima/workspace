a57137a458d64338ff72687dd01746a9f7187b38
Yusuke Nojima <nojima@ynojima.com>
2024-05-11 22:12:10 +0900

    Add test

--- algorithm-w/src/main.rs
+++ algorithm-w/src/main.rs
@@ -47,3 +47,38 @@ fn do_eval(input: &str) -> anyhow::Result<()> {
     println!("Value = {:?}", value);
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::rc::Rc;
+
+    use crate::{eval, lexer, parser, value::{Frame, Value}};
+
+    fn doit(input: &str) -> anyhow::Result<Value> {
+        let token_and_spans = lexer::lex(input)?;
+        let tokens: Vec<_> = token_and_spans.into_iter().map(|(t, _)| t).collect();
+        let ast = parser::parse(&tokens)?;
+        let frame = Frame {
+            parent: None,
+            v_name: "false".into(),
+            v_value: Value::Bool(false),
+        };
+        let value = eval::eval(&ast, Rc::new(frame))?;
+        Ok(value)
+    }
+
+    #[test]
+    fn test() -> anyhow::Result<()> {
+        assert_eq!(Value::Integer(42), doit("42")?);
+        assert_eq!(Value::Integer(7), doit("3 + 4")?);
+        assert_eq!(Value::Bool(true), doit("true")?);
+        assert_eq!(Value::Bool(false), doit("false")?);
+        assert_eq!(Value::Bool(true), doit("0 == 0")?);
+        assert_eq!(Value::Bool(false), doit("0 == 1")?);
+        assert_eq!(Value::Integer(2), doit("if 1 == 1 then 2 else 3")?);
+        assert_eq!(Value::Integer(3), doit("if 1 == 2 then 2 else 3")?);
+        assert_eq!(Value::Integer(42), doit("(lambda x. x) 42")?);
+        assert_eq!(Value::Integer(5), doit("(lambda x. lambda y. x+y+y) 1 2")?);
+        Ok(())
+    }
+}
