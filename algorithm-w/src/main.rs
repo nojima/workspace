509d312064cfaee06791755a8124815bbaf1e5a8
Yusuke Nojima <nojima@ynojima.com>
2024-05-11 19:13:43 +0900

    Use rustyline

--- algorithm-w/src/main.rs
+++ algorithm-w/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::stdin;
+use rustyline::{error::ReadlineError, DefaultEditor};
 
 mod ast;
 mod lexer;
@@ -8,14 +8,29 @@ mod symbol;
 mod token;
 
 fn main() -> anyhow::Result<()> {
-    let mut line = String::new();
+    let mut rl = DefaultEditor::new()?;
     loop {
-        line.clear();
-        stdin().read_line(&mut line)?;
-
-        let token_and_spans = lexer::lex(&line)?;
-        let tokens: Vec<_> = token_and_spans.into_iter().map(|(t, _)| t).collect();
-        let ast = parser::parse(&tokens)?;
-        println!("{:?}", ast);
+        let readline = rl.readline("expr> ");
+        let line = match readline {
+            Ok(line) => line,
+            Err(ReadlineError::Eof) => return Ok(()),
+            Err(e) => {
+                eprintln!("{e}");
+                continue;
+            }
+        };
+        let _ = rl.add_history_entry(line.as_str());
+        if let Err(e) = eval(&line) {
+            eprintln!("{e}");
+            continue;
+        }
     }
+}
+
+fn eval(input: &str) -> anyhow::Result<()> {
+    let token_and_spans = lexer::lex(input)?;
+    let tokens: Vec<_> = token_and_spans.into_iter().map(|(t, _)| t).collect();
+    let ast = parser::parse(&tokens)?;
+    println!("{:?}", ast);
+    Ok(())
 }
