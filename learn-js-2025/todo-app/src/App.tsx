4b12cf329acca236ded74c460a8435a0cb1c8f3e
Yusuke Nojima <nojima@ynojima.com>
2025-01-29 14:48:18 +0900

    [learn-js-2025] new app

---
+++ learn-js-2025/todo-app/src/App.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { createTheme, MantineProvider } from '@mantine/core'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const theme = createTheme({})
+
+function App(): JSX.Element {
+  const [count, setCount] = useState(0)
+
+  return (
+    <MantineProvider theme={theme}>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </MantineProvider>
+  )
+}
+
+export default App
