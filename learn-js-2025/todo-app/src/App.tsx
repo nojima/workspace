aaec91af599fcbebce4d1db9ff0231d1d2ba00d0
Yusuke Nojima <nojima@ynojima.com>
2025-01-29 14:52:29 +0900

    [learn-js-2025] remove unused css and images

--- learn-js-2025/todo-app/src/App.tsx
+++ learn-js-2025/todo-app/src/App.tsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 import { createTheme, MantineProvider } from '@mantine/core'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 const theme = createTheme({})
 
@@ -11,14 +8,6 @@ function App(): JSX.Element {
 
   return (
     <MantineProvider theme={theme}>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
