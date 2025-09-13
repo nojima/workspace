816797c644a68616165b0bc79700750438c40c0f
Yusuke Nojima <nojima@ynojima.com>
2025-09-13 20:36:57 +0900

    axum-server-example: use Shared

--- axum-server-example/src/main.rs
+++ axum-server-example/src/main.rs
@@ -1,13 +1,30 @@
+//! ```cargo
+//! [dependencies]
+//! axum-server = "0.7"
+//! hyper = "1"
+//! tokio = { version = "1", features = ["rt-multi-thread", "signal", "macros"] }
+//! tower = { version = "0.5", features = ["make", "util"] }
+//! ```
 use std::{convert::Infallible, net::SocketAddr, time::Duration};
 
 use axum_server::Handle;
 use hyper::{Request, Response, body::Incoming};
+use tower::make::Shared;
+
+async fn request_handler(req: Request<Incoming>) -> Result<Response<String>, Infallible> {
+    eprintln!("HTTP request received: {req:?}");
+    Ok(Response::new("Hello, World!".to_owned()))
+}
+
+async fn signal_handler(handle: Handle) {
+    tokio::signal::ctrl_c().await.unwrap();
+    eprintln!("SIGINT received!");
+    handle.graceful_shutdown(Some(Duration::from_secs(10)));
+}
 
 #[tokio::main]
 async fn main() {
-    let make_service = tower::service_fn(async |_: SocketAddr| -> Result<_, Infallible> {
-        Ok(tower::service_fn(hello_handler))
-    });
+    let make_service = Shared::new(tower::service_fn(request_handler));
 
     let handle = Handle::new();
     tokio::spawn(signal_handler(handle.clone()));
@@ -19,15 +36,4 @@ async fn main() {
         .serve(make_service)
         .await
         .unwrap();
-}
-
-async fn hello_handler(req: Request<Incoming>) -> Result<Response<String>, Infallible> {
-    eprintln!("HTTP request received: {req:?}");
-    Ok(Response::new("Hello, World!".to_owned()))
-}
-
-async fn signal_handler(handle: Handle) {
-    tokio::signal::ctrl_c().await.unwrap();
-    eprintln!("SIGINT received!");
-    handle.graceful_shutdown(Some(Duration::from_secs(10)));
 }
